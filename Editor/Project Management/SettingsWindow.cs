3f64aa8e8dc0000173fc17ae035041fc9c5bcc5c
CompilerDestroyer <alcarinpower@gmail.com>
2025-03-13 08:41:12 +0300

    -

--- Editor/Project Management/SettingsWindow.cs
+++ Editor/Project Management/SettingsWindow.cs
@@ -11,7 +11,6 @@ namespace CompilerDestroyer.Editor.ToolsManager
     public sealed class EditorToolsSettings : EditorWindow
     {
         private const string documentationName = "Documentation";
-        private const string folderIconsName = "Folder Icons";
         private const string scriptDescriptionName = "Script Description";
 
         private static readonly Vector2 minWindowSize = new Vector2(310f, 200f);
@@ -26,26 +25,13 @@ namespace CompilerDestroyer.Editor.ToolsManager
             settingsWindow.titleContent.text = "Editor Tools Settings";
             settingsWindow.titleContent.image = EditorGUIUtility.FindTexture("SettingsIcon");
             settingsWindow.minSize = minWindowSize;
-            
-            //if (!SessionState.GetBool("IsCompilerButcherWindowInitiated", false))
-            //{
-            //    Rect mainEditorWindowRect = EditorGUIUtility.GetMainWindowPosition();
-            //    Debug.Log("hee");
-            //    // Calculate the center position for the editor window
-            //    float centerX = mainEditorWindowRect.x + (mainEditorWindowRect.width - 734f) / 2;
-            //    float centerY = mainEditorWindowRect.y + (mainEditorWindowRect.height - 438f) / 2;
-
-            //    settingsWindow.position = new Rect(centerX, centerY, 734f, 438f);
-
-            //    SessionState.SetBool("IsCompilerButcherWindowInitiated", true);
-            //}
         }
 
         public void CreateGUI()
         {
 
-            TreeViewItemData<string> scriptDescriptionSetting = new TreeViewItemData<string>(1, scriptDescriptionName);
             TreeViewItemData<string> documentationSetting = new TreeViewItemData<string>(2, documentationName);
+            TreeViewItemData<string> scriptDescriptionSetting = new TreeViewItemData<string>(1, scriptDescriptionName);
 
 
             rootDict.Add(documentationName, null);
@@ -58,57 +44,6 @@ namespace CompilerDestroyer.Editor.ToolsManager
             SettingsPanel settingsWindow = new SettingsPanel(ref projectSettingsList, ref rootDict);
 
             rootVisualElement.Add(settingsWindow);
-        }
-
-
-
-        private void ApplyButton()
-        {
-            if (GUILayout.Button("Apply!"))
-            {
-                UnityEditor.Compilation.CompilationPipeline.RequestScriptCompilation();
-                AssetDatabase.Refresh();
-            }
-        }
-        private void EnableOrDisableProjects(ref bool toggle, string toggleName, string findAssetFilter, ref string[] searchInFolders,
-                                bool hasExtraSettings = false, Action extraEditorSettings = null)
-        {
-            string scriptContent;
-
-            if (toggle)
-            {
-                string[] currentPath = AssetDatabase.FindAssets(findAssetFilter, searchInFolders);
-                Debug.Log(currentPath.Length);
-
-                for (int i = 0; i < currentPath.Length; i++)
-                {
-                    string editorScriptName = AssetDatabase.GUIDToAssetPath(currentPath[i]).Replace("\\", "/");
-                    scriptContent = File.ReadAllText(editorScriptName);
-
-                    string textToAddAtStart = "/*";
-                    string textToAddAtEnd = "*/";
-
-
-                    string newContent = textToAddAtStart + scriptContent + textToAddAtEnd;
-
-                    File.WriteAllText(editorScriptName, newContent);
-                }
-            }
-            else
-            {
-                string[] currentPath = AssetDatabase.FindAssets(findAssetFilter, searchInFolders);
-                for (int i = 0; i < currentPath.Length; i++)
-                {
-                    string editorScriptName = AssetDatabase.GUIDToAssetPath(currentPath[i]).Replace("\\", "/");
-
-                    scriptContent = File.ReadAllText(editorScriptName);
-
-
-                    string newContent = scriptContent[2..^2];
-
-                    File.WriteAllText(editorScriptName, newContent);
-                }
-            }
         }
     }
 }
